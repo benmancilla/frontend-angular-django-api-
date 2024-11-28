ed554a1b30ebf6e4986d781194f498d368f96f9d
matias riveros <134396611+Extopus@users.noreply.github.com>
2024-11-28 17:06:01 -0300

    PLUGIN ZOOM
    Se agrega un plugin en el inicio de sesion que hace zoom y lo restablece

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,9 +1,9 @@
-// login.page.ts
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
-
+import { Keyboard } from '@capacitor/keyboard';  // Importamos el plugin del teclado
+import { TextZoom } from '@capacitor/text-zoom';
 
 @Component({
   selector: 'app-login',
@@ -24,13 +24,16 @@ export class LoginPage {
     });
   }
 
+  // Se ejecuta al hacer clic en el botón de login
   async onLogin() {
     const { username, password } = this.loginForm.value;
     const storedPassword = localStorage.getItem('password') || 'contra'; 
 
     localStorage.setItem('username', username);
+    
     if (username === 'ben' && password === storedPassword) {
       this.router.navigate(['/home']);
+      Keyboard.hide();  // Ocultar el teclado después de enviar el formulario
     } else {
       const alert = await this.alertController.create({
         header: 'Error de validación',
@@ -38,10 +41,37 @@ export class LoginPage {
         buttons: ['OK']
       });
       await alert.present();
+      Keyboard.hide();  // Ocultar el teclado si las credenciales son incorrectas
     }
   }
 
+  // Redirige a la página de recuperación de contraseña
   goToPasswordReset() {
     this.router.navigate(['/passreset']);
+  }
+
+  // Método para aumentar el tamaño del texto
+  async increaseTextZoom() {
+    try {
+      const result = await TextZoom.get();
+      const newZoom = result.value + 0.1; // Aumentamos el zoom en 0.1
+      await TextZoom.set({ value: newZoom });
+    } catch (error) {
+      console.error('Error aumentando el zoom:', error);
+    }
+  }
+
+  // Método para restablecer el zoom a su valor predeterminado
+  async resetTextZoom() {
+    try {
+      await TextZoom.set({ value: 1 }); // Establecemos el zoom a 100%
+    } catch (error) {
+      console.error('Error restableciendo el zoom:', error);
+    }
+  }
+
+  // Método para ocultar el teclado cuando el usuario haga clic fuera del formulario
+  dismissKeyboard() {
+    Keyboard.hide();
   }
 }
